36f1d609522e937fea3e1f9b5ef803e27345bf2c
Jack Boswell (boswelja) <boswelja@outlook.com>
2017-06-29 18:11:05 +1200

    Side nav

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,3 +11,36 @@ $(document).ready(function(){
     }
   });
 });
+$(window).scroll(function(){
+    // This is then function used to detect if the element is scrolled into view
+    function elementScrolled(elem)
+    {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    // This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
+    if(elementScrolled('#lexussrealms')) {
+      document.getElementById('home-nav').className = "active-list-item"
+      document.getElementById('discord-nav').className = "inactive-list-item"
+      document.getElementById('faq-nav').className = "inactive-list-item"
+      document.getElementById('contact-staff-nav').className = "inactive-list-item"
+    } else if(elementScrolled('#discord')) {
+      document.getElementById('home-nav').className = "inactive-list-item"
+      document.getElementById('discord-nav').className = "active-list-item"
+      document.getElementById('faq-nav').className = "inactive-list-item"
+      document.getElementById('contact-staff-nav').className = "inactive-list-item"
+    } else if(elementScrolled('#faq')) {
+      document.getElementById('home-nav').className = "inactive-list-item"
+      document.getElementById('discord-nav').className = "inactive-list-item"
+      document.getElementById('faq-nav').className = "active-list-item"
+      document.getElementById('contact-staff-nav').className = "inactive-list-item"
+    } else if(elementScrolled('#contact-staff')) {
+      document.getElementById('home-nav').className = "inactive-list-item"
+      document.getElementById('discord-nav').className = "inactive-list-item"
+      document.getElementById('faq-nav').className = "inactive-list-item"
+      document.getElementById('contact-staff-nav').className = "active-list-item"
+    }
+});
